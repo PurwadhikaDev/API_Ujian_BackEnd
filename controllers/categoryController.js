e6c7fe053638d49156d78fe5cbafc8275ab7cd10
Baron Hartono <baronhartono@gmail.com>
2020-02-10 10:59:13 +0700

    c

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -32,15 +32,28 @@ module.exports = {
         });
     },
     addCategory: (req,res) => {
-        const query = `INSERT INTO categories SET ? ;`
-
-        sqlDB.query(query, req.body, (err,results) => {
+        let query = `SELECT id 
+                FROM categories 
+                WHERE category = ${sqlDB.escape(req.body.category)};`;
+        sqlDB.query(query, (err,results) => {
             if(err) {
                 return res.status(500).send(err)
             }
+            
+            if(results.length > 0) {
+                return res.status(500).send({ message: 'Category name already exist!'})
+            }
+            
+            query = `INSERT INTO categories SET ? ;`
 
-            res.status(200).send(results)
-        })
+            sqlDB.query(query, req.body, (err,results) => {
+                if(err) {
+                    return res.status(500).send(err)
+                }
+
+                res.status(200).send(results)
+            })
+        })    
     },
     editCategory: (req,res) => {
         const query = `UPDATE categories SET ? WHERE id = ${sqlDB.escape(req.params.id)}`
